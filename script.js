f9c1edb69b004c24675f1589c0066497ef05a431
Wagner Rafael | AIExxplorer <132309825+AIExxplorer@users.noreply.github.com>
2024-10-04 08:48:48 -0300

    fix: update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const fetch = require('node-fetch'); // Certifique-se de que node-fetch está instalado como dependência.
+const fetch = require('node-fetch'); // Certifique-se de que node-fetch está instalado como dependência
 
+// O token é acessado a partir da variável de ambiente configurada no workflow
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 async function fetchGithubMembers(page = 1) {
     const response = await fetch(`https://api.github.com/orgs/Artificial-Universe/members?per_page=100&page=${page}`, {
         headers: {
-            Authorization: `token ${GITHUB_TOKEN}`
+            Authorization: `token ${GITHUB_TOKEN}`  // O token é passado nos headers da requisição
         }
     });
     const githubMembers = await response.json();
