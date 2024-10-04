d7696bb72c30c18448b3844501ad4b644d1cd0ab
Wagner Rafael | AIExxplorer <132309825+AIExxplorer@users.noreply.github.com>
2024-10-04 08:01:05 -0300

    chore: create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch'); // Certifique-se de que node-fetch está instalado como dependência.
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+async function fetchGithubMembers(page = 1) {
+    const response = await fetch(`https://api.github.com/orgs/Artificial-Universe/members?per_page=100&page=${page}`, {
+        headers: {
+            Authorization: `token ${GITHUB_TOKEN}`
+        }
+    });
+    const githubMembers = await response.json();
+    return githubMembers;
+}
+
+async function loadTeamMembers() {
+    let githubMembers = [];
+    let page = 1;
+    let moreMembers = true;
+
+    while (moreMembers) {
+        const members = await fetchGithubMembers(page);
+        if (members.length === 0) {
+            moreMembers = false;
+        } else {
+            githubMembers = githubMembers.concat(members);
+            page++;
+        }
+    }
+
+    console.log("Membros da Organização:");
+    githubMembers.forEach(member => {
+        console.log(`Login: ${member.login}, URL: ${member.html_url}`);
+    });
+}
+
+loadTeamMembers().catch(error => console.error('Erro ao carregar membros:', error));
